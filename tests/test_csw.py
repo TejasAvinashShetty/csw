5e8a67c6699ca1bc0e1992c9541b968dc6e89b77
Tejas Shetty <tejasshetty.1808@gmail.com>
2020-01-22 23:25:49 +0530

    Add test for output write
    I wish to ensure that the output for a given
    test list of strings is same as what I would
    expect.

--- tests/test_csw.py
+++ tests/test_csw.py
@@ -11,3 +11,20 @@ def test_valid_version():
     v_curr = parse_version(csw.__version__)
     v_orig = parse_version("0.1.0")
     assert v_curr >= v_orig
+
+
+def test_file_write():
+    t1 = ['x', 'y', '1423', 'randar', 'tzz']
+    t1n = ['x', 'y', '1423', 'randar', 'tzz']
+    csw.csw('t1', t1, t1n)
+
+    with open('t1-test.txt', r) as test:
+        test_output = test.readlines()
+
+    with open('t1.txt', r) as actual:
+        actual_output = actual.readlines()
+
+    for test_line, actual_line in zip(test_output, actual_output):
+        assert test_line == actual_line
+
+    pass
